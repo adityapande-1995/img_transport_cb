8e9d3ba3a0e4dca5b650ad37fbd8bb8d545f68bf
Aditya Pande <adityapande@google.com>
2023-06-23 22:59:57 +0000

    Added basic structure of publisher and subscriber
    Signed-off-by: Aditya Pande <adityapande@google.com>

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # img_transport_cb
+This temporary repo is used to demonstrate the use of callback groups with image transport.
+
+# To build
+Source your ros2 rolling distribution, install dependencies of this package using rosdep, and run : 
+```
+mkdir build
+cd build
+cmake ..; make
+```
+
+# To run
+Download any test image and place it at the root as say `some_image.jpeg`
+In terminal 1, run :
+```
+./build/my_publisher some_image.jpeg
+```
+
+In terminal 2 :
+```
+./build/my_subscriber
+```
+
+Work in progress :
+This should run the 2 callbacks that are mutually exclusive in the selected callback queue. By default these are mutually exclusive, so the sleep() in
+the second callback blocks the execution of the original callback.
+
+Changing the group type to reentrant and changing the executor to multithreaded should fix the issue. (Need to test this)
+
---
+++ src/my_subscriber.cpp
@@ -0,0 +1,59 @@
+// Copyright 2021, Open Source Robotics Foundation, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "cv_bridge/cv_bridge.hpp"
+#include "image_transport/image_transport.hpp"
+#include "opencv2/highgui.hpp"
+#include "rclcpp/logging.hpp"
+#include "rclcpp/rclcpp.hpp"
+#include "sensor_msgs/msg/image.hpp"
+
+#include <unistd.h>
+
+void imageCallback(const sensor_msgs::msg::Image::ConstSharedPtr & msg)
+{
+  try {
+    cv::imshow("view", cv_bridge::toCvShare(msg, "bgr8")->image);
+    cv::waitKey(10);
+  } catch (const cv_bridge::Exception & e) {
+    auto logger = rclcpp::get_logger("my_subscriber");
+    RCLCPP_ERROR(logger, "Could not convert from '%s' to 'bgr8'.", msg->encoding.c_str());
+  }
+}
+
+void imageCallback2(const sensor_msgs::msg::Image::ConstSharedPtr & msg)
+{
+  auto logger = rclcpp::get_logger("my_subscriber");
+  sleep(10);
+  RCLCPP_INFO(logger, "Hello there !");
+}
+int main(int argc, char ** argv)
+{
+  rclcpp::init(argc, argv);
+  rclcpp::NodeOptions options;
+  rclcpp::Node::SharedPtr node = rclcpp::Node::make_shared("image_listener", options);
+  auto cb_group = node->create_callback_group(rclcpp::CallbackGroupType::MutuallyExclusive);
+
+  cv::namedWindow("view");
+  cv::startWindowThread();
+  image_transport::ImageTransport it(node);
+
+  image_transport::Subscriber sub = it.subscribe("camera/image", 1, imageCallback, cb_group);
+  image_transport::Subscriber sub2 = it.subscribe("camera/image", 1, imageCallback2, cb_group);
+
+  rclcpp::spin(node);
+  cv::destroyWindow("view");
+
+  return 0;
+}
